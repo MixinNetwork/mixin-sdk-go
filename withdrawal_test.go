f33574e12e1209911443a4615409d74d8515c1e1
LiYuqing <im.yuqlee@gmail.com>
2024-03-05 19:05:05 +1100

    Fix test

--- withdrawal_test.go
+++ withdrawal_test.go
@@ -1,6 +1,7 @@
 package kernel
 
 import (
+	"encoding/hex"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -18,4 +19,11 @@ func TestWithdrawal(t *testing.T) {
 	require.Len(tx.Outputs, 1)
 	require.Equal("876c0df9fdcac099c98b7b848dc92e53cff23ea1f84b7cdf04c7a475e661efc0", tx.Snapshot)
 	require.Equal(uint8(5), tx.Version)
+
+	tx, err = mn.GetWithdrawalClaim("2e04fba82c1f874602a7baf1e5d0d56b6dfdc28b8af1c43b368ba1f5654aada6")
+	require.Nil(err)
+	require.NotNil(tx)
+	require.Equal("b6fbe6f7d34906725c7a54e89c95c07717dee7cab033b3e6d69db485bbcb3089", tx.Hash)
+	extra, _ := hex.DecodeString(tx.Extra)
+	require.Equal("658aace0c5e2291ca91f2dd734dc533db68c447f78f2428682349396084dc4cc", string(extra[64:]))
 }
